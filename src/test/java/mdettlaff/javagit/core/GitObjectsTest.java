02151e56a26e2735264b95236e4d5a24dad9a8ac
Michał Dettlaff <mdettlaff@jitsolutions.pl>
2013-05-02 16:38:00 +0200

    test writing blob

--- src/test/java/mdettlaff/javagit/core/GitObjectsTest.java
+++ src/test/java/mdettlaff/javagit/core/GitObjectsTest.java
@@ -1,12 +1,15 @@
 package mdettlaff.javagit.core;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 
+import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 
 public class GitObjectsTest {
@@ -24,5 +27,19 @@ public class GitObjectsTest {
 		assertEquals(8, result.getSize());
 		assertTrue(result.getContent() instanceof Blob);
 		assertEquals("foo\nbar\n", new String(((Blob) result.getContent()).getContent()));
+	}
+
+	@Test
+	public void testWriteBlob() throws Exception {
+		Filesystem filesystem = mock(Filesystem.class);
+		ByteArrayOutputStream blob = new ByteArrayOutputStream();
+		when(filesystem.openOutput(".git/objects/3b/d1f0e29744a1f32b08d5650e62e2e62afb177c")).thenReturn(blob);
+		GitObjects objects = new GitObjects(filesystem);
+		GitObject object = new GitObject(GitObject.Type.BLOB, 8, new Blob("foo\nbar\n".getBytes()));
+		// exercise
+		objects.write(object);
+		// verify
+		byte[] expectedBlob = IOUtils.toByteArray(getClass().getResourceAsStream("blob"));
+		assertArrayEquals(expectedBlob, blob.toByteArray());
 	}
 }
