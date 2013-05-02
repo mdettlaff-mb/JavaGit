b92ec3607cf250278ad82231564fbb2b92e34a79
Michał Dettlaff <mdettlaff@jitsolutions.pl>
2013-05-02 16:55:10 +0200

    jar with dependencies

--- src/main/java/mdettlaff/javagit/command/CatFile.java
+++ src/main/java/mdettlaff/javagit/command/CatFile.java
@@ -2,6 +2,8 @@ package mdettlaff.javagit.command;
 
 import java.io.IOException;
 
+import com.google.common.base.Preconditions;
+
 import mdettlaff.javagit.core.GitObject;
 import mdettlaff.javagit.core.GitObjects;
 import mdettlaff.javagit.core.ObjectId;
@@ -15,6 +17,7 @@ public class CatFile implements Command {
 	}
 
 	public void execute(String[] args) throws IOException {
+		Preconditions.checkArgument(args.length > 0, "Object ID parameter is required");
 		ObjectId id = new ObjectId(args[0]);
 		GitObject object = objects.read(id);
 		System.out.print(object);
